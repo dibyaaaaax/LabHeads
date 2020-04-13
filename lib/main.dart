9040f6168fe0a0dd25fb05061c103146d63d83e7
mohnish2011 <43900817+mohnish2011@users.noreply.github.com>
2020-04-14 03:02:34 +0400

    UI/UX Design changed

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [Color.fromRGBO(2, 0, 36, 1),Color.fromRGBO(25, 0, 100, 1.0)]
-        )
-      ),
-      
+      // decoration: BoxDecoration(
+      //   gradient: LinearGradient(
+      //     begin: Alignment.centerLeft,
+      //     end: Alignment.centerRight,
+      //     colors: [Color.fromRGBO(2, 0, 36, 1),Color.fromRGBO(255,0,247,1)]
+      //   )
+      // ),
+      color: Color.fromRGBO(42, 54, 63, 1),
     child: Material(
       type: MaterialType.transparency,
       child: Row(
@@ -45,26 +45,27 @@ class MyHomePage extends StatelessWidget {
             children: <Widget>[
               Image.asset(
                 "assets/images/labmanageicon.png",
-                width: 300,
-                height: 400,
+                width: 250,
+                height: 300,
               ),
-              Text(
-                "LabHeads",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white54
-                )
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(width:340),
+                  Image.asset(
+                    "images/head.png",
+                    width: 150,
+                    height: 200
+                    )
             ],
           ),
-          Center(
-            child: VerticalDivider(
-              color: Color.fromRGBO(237, 237, 237, 0.7),
-              thickness: 2,
-              indent: 200,
-              endIndent: 200,
-            )
+          ]
+          ),
+          VerticalDivider(
+            color: Color.fromRGBO(237, 237, 237, 0.5),
+            thickness: 1,
+            indent: 200,
+            endIndent: 200,
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -73,8 +74,8 @@ class MyHomePage extends StatelessWidget {
             ],
           )
         ]
-      )
     ),
+    )
     );
   }
 }
@@ -88,7 +89,10 @@ class Form extends StatefulWidget {
 
 class _FormState extends State<Form> {
 
-  _button(text) => RaisedButton(
+  _button(text) => Container(
+    width: 400,
+    height: 60,
+    child: RaisedButton(
       onPressed: () {
         Navigator.push(
           context,
@@ -96,42 +100,58 @@ class _FormState extends State<Form> {
         );
       },
       textColor: Colors.black,
-      color: Colors.lightBlue,
-      
+      color: Color.fromRGBO(110, 217, 160, 1),
+  
+      shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none
+          ),
       child: new Text(
         text,
-        style: TextStyle(fontFamily: 'RobotoMono'),
+        style: TextStyle(
+          fontFamily: 'RobotoMono',
+          fontWeight: FontWeight.bold,
+          fontSize: 18
+          ),
       ),
         
+   )
   );
 
-  _textBox(text) =>TextField(
-    decoration: InputDecoration(
-      fillColor: Colors.white,
-      filled: true,
-      labelText: text,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(10),
-        borderSide: BorderSide.none
-        )
-    ),
+  _textBox(text) =>Container(
+    width: 400,
+    child: TextField(
+      cursorColor: Colors.black12,
+      decoration: InputDecoration(
+        // fillColor: Colors.white,
+        // filled: true,
+        labelText: text,
+        labelStyle: TextStyle(
+          color: Colors.white30
+        ),
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Color.fromRGBO(110, 217, 160, 1))
+          )
+      ),
+    )
   );
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
+      width: 700,
       height: 400,
       child: Column(
          mainAxisAlignment: MainAxisAlignment.center,
          
          children: [
           _textBox("Enter Username"),
-          SizedBox(height: 10),
+          SizedBox(height: 30),
           _textBox("Enter Password"),
-          SizedBox(height: 10),
-          _button("Log In"),
+          SizedBox(height: 30),
+          _button("LOGIN"),
          ],
        ),
     );
